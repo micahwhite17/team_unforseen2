2bb5772de5a9c71202327a2f1dea3c8a8cffa826
micahwhite17 <38734438+micahwhite17@users.noreply.github.com>
2018-04-25 16:38:51 -0500

    ss

--- app.py
+++ app.py
@@ -11,55 +11,47 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # Webhook
-	@app.route('/', methods=['POST'])
-	def webhook():
-		data = request.get_json()
-		log('Recieved {}'.format(data))
-		msg = ''
-		if (data['name'] == 'He Died (AND RES)urrected') or (data['name'] == 'Because He LIVes') or (data['name'] == 'Lauren Bierman') or (data['name'] == 'Grace, God\'s gift to man') or (data['name'] == 'Made in his iMAGe'): # the bot only mirrors one user
-		    text = data['text']
-		    time.sleep(1)
-		    send_message(mock(text))
-		if (data['name'] == 'Keaton Immekus') or data['name'] == 'Aaron Leader of the Israelites') or data['name'] == 'Ja Son of God'):
-			text = data['text']
- 		    time.sleep(1)
- 		    send_message(mock(text))
-		if (data['name'] == 'Micah White'):
-			text = data['text']
- 		    time.sleep(1)
- 		    send_message(mock(text))
-	  return "ok", 200
+@app.route('/', methods=['POST'])
+def webhook():
+  data = request.get_json()
+  log('Recieved {}'.format(data))
+  msg = ''
+  if (data['name'] == 'Micah White') or (data['sender_id'] == 'PUT_SENDER_ID_HERE'): # the bot only mirrors one user
+    text = data['text']
+    time.sleep(1)
+    send_message(mock(text))
+  return "ok", 200
 
-	def mock(text, diversity_bias=0.5, random_seed=None):
-	  # Error handling
-	  if diversity_bias < 0 or diversity_bias > 1:
-	    raise ValueError('diversity_bias must be between the inclusive range [0,1]')
-	  # Seed the random number generator
-	  random.seed(random_seed)
-	  # Mock the text
-	  out = ''
-	  last_was_upper = True
-	  swap_chance = 0.5
-	  for c in text:
-	    if c.isalpha():
-	      if random.random() < swap_chance:
-	        last_was_upper = not last_was_upper
-	        swap_chance = 0.5
-	      c = c.upper() if last_was_upper else c.lower()
-	      swap_chance += (1-swap_chance)*diversity_bias
-	    out += c
-	  return out
+def mock(text, diversity_bias=0.5, random_seed=None):
+  # Error handling
+  if diversity_bias < 0 or diversity_bias > 1:
+    raise ValueError('diversity_bias must be between the inclusive range [0,1]')
+  # Seed the random number generator
+  random.seed(random_seed)
+  # Mock the text
+  out = ''
+  last_was_upper = True
+  swap_chance = 0.5
+  for c in text:
+    if c.isalpha():
+      if random.random() < swap_chance:
+        last_was_upper = not last_was_upper
+        swap_chance = 0.5
+      c = c.upper() if last_was_upper else c.lower()
+      swap_chance += (1-swap_chance)*diversity_bias
+    out += c
+  return out
 
-	# DO NOT MODIFY ANYTHING BELOW THIS LINE
-	def send_message(msg):
-	  url  = 'https://api.groupme.com/v3/bots/post'
-	  data = {
-	          'bot_id' : os.getenv('GROUPME_BOT_ID'),
-	          'text'   : msg,
-	         }
-	  request = Request(url, urlencode(data).encode())
-	  json = urlopen(request).read().decode()
+# DO NOT MODIFY ANYTHING BELOW THIS LINE
+def send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
+  data = {
+          'bot_id' : os.getenv('GROUPME_BOT_ID'),
+          'text'   : msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
 
-	def log(msg):
-	  print(str(msg))
-	  sys.stdout.flush()
+def log(msg):
+  print(str(msg))
+  sys.stdout.flush()
