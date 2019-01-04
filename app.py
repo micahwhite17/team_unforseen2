35634c1f4ff9abafccffb2754537b8334223aa2b
micahwhite17 <micah.white@live.com>
2019-01-03 19:22:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,24 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+ENABLED = False;
+
 # Webhook
 @app.route('/', methods=['POST'])
 def webhook():
-  data = request.get_json()
-  log('Recieved {}'.format(data))
-  msg = ''
-  #have Aaron, andres, jason, keaton
-  # if (data['sender_id'] == '22498083') or (data['sender_id'] == '41870459') or (data['sender_id'] == '39819849') or (data['name'] == 'Made in His iMAGe') or (data['sender_id'] == '32642417'):
-  #   text = data['text']
-  #   time.sleep(1)
-  #   send_message(mock(text))
-  if (data['sender_id'] == '32642417'):# or (data['name'] == 'Lauren Bierman') or (data['name'] == 'Because He LIVes'):
-    #text = data['text']
-    time.sleep(1)
-    send_message('Ohio sucks.')
+  if(ENABLED):
+	  data = request.get_json()
+	  log('Recieved {}'.format(data))
+	  msg = ''
+	  #have Aaron, andres, jason, keaton
+	  # if (data['sender_id'] == '22498083') or (data['sender_id'] == '41870459') or (data['sender_id'] == '39819849') or (data['name'] == 'Made in His iMAGe') or (data['sender_id'] == '32642417'):
+	  #   text = data['text']
+	  #   time.sleep(1)
+	  #   send_message(mock(text))
+	  if (data['sender_id'] == '32642417'):# or (data['name'] == 'Lauren Bierman') or (data['name'] == 'Because He LIVes'):
+		#text = data['text']
+		time.sleep(1)
+		send_message('Ohio sucks.')
   return "ok", 200
 
 def mock(text, diversity_bias=0.5, random_seed=None):
